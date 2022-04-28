3c7cb80b61b3078664d2c99ab0578ac9a6da3a0d
Lucas Spencer <lucas.spencer14@gmail.com>
2022-04-28 16:41:25 -0600

    css

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path')
  
 const app = express()
  
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '../index.html'))})
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '../public/index.html'))})
  
 const port = process.env.PORT || 4545
  
